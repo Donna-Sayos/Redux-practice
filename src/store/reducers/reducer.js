acc13e2716383c229e7ee985c58ca1f0fd2a63b3
Donna-Sayos <Donnasayos@gmail.com>
2023-04-22 14:14:53 -0400

    added store folder

---
+++ src/store/reducers/reducer.js
@@ -0,0 +1,19 @@
+let lastId = 0;
+
+function reducer(state = [], action) {
+  switch (action.type) {
+    case "BUG_ADDED":
+      return [
+        ...state, // spread operator to copy the state array
+        {
+          id: ++lastId,
+          description: action.payload.description,
+          resolved: false,
+        },
+      ];
+    case "BUG_REMOVED":
+      return state.filter((bug) => bug.id !== action.payload.id); // filter out the bug with the id that matches the payload id
+    default: // if the action type is none of these values, return the state
+      return state;
+  }
+}

9ed34fc911ccc662c046dc5260c72e9175539390
Donna-Sayos <Donnasayos@gmail.com>
2023-04-22 14:43:31 -0400

    used configureStore instead of depracated createStore

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,16 @@
-
+import { configureStore } from "@reduxjs/toolkit";
 import { createLogger } from "redux-logger";
 import { composeWithDevTools } from "redux-devtools-extension";
 import reducer from "./reducers/reducer";
 
+const middleware = createLogger({ collapsed: true });
+
+const store = configureStore({
+  reducer,
+  middleware,
+  devTools: process.env.NODE_ENV !== "production",
+  enhancers:
+    process.env.NODE_ENV !== "production" ? [composeWithDevTools()] : [], // if the environment is not production, use the composeWithDevTools
+});
 
 export default store;

be9eef2f3c7855cdae27bc5372c37881af44fef4
Donna-Sayos <Donnasayos@gmail.com>
2023-04-23 19:10:49 -0400

    server done just need to make database and routes

--- server/app.js
+++ server/app.js
@@ -1,4 +1,49 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const path = require("path");
+const logger = require("./logger/index");
+
+dotenv.config({ path: "./server/config/config.env" });
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(logger);
+app.use(express.static(path.join(__dirname, "..", "dist")));
+
+//routes
+
+// when the ROOT url path is requested, this sends the index.html file to the client
+app.use("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "dist/index.html"));
+});
+
+// prevents unauthorized access to files on the server
+app.use((req, res, next) => {
+  if (path.extname(req.path).length > 0) {
+    const error = new Error("Not fount 🫤");
+    error.status = 404;
+    next(error);
+  } else {
+    next();
+  }
+});
+
+// handler for ALL url paths that do not match any routes
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "dist/index.html")); // just send the index.html file to the client
+});
+
+//error handler
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(status).send(message);
+});
+
+module.exports = app;
---
+++ server/index.js
@@ -0,0 +1,21 @@
+const chalk = require("chalk");
+const app = require("./app");
+
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () => {
+  console.log(
+    chalk.cyanBright(
+      `Server running in ${chalk.blueBright(
+        process.env.NODE_ENV
+      )} mode on PORT: ${chalk.blueBright(PORT)} 🔊🔊🔊`
+    )
+  );
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(chalk.redBright(`Error: ${err.message}`));
+  console.log("Shutting down server due to Unhandled Promise Rejection");
+  server.close(() => {
+    process.exit(1); // 1 is for unhandled rejection
+  });
+});

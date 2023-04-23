184f8291c4c77ed1f0e9a7527807e80337d8efcf
Donna-Sayos <Donnasayos@gmail.com>
2023-04-23 12:37:29 -0400

    added react

--- src/index.js
+++ src/index.js
@@ -1,18 +1,1 @@
 import store from "./store";
-import {
-  bugCreated,
-  bugResolved,
-  bugRemoved,
-} from "./store/actions/actionCreators";
-
-const unsubscribe = store.subscribe(() => {
-  console.log("STORE CHANGED: ", store.getState());
-});
-
-store.dispatch(bugCreated("Bug #1"));
-store.dispatch(bugResolved(1));
-store.dispatch(bugRemoved(1));
-
-unsubscribe();
-
-console.log("STORE STATE: ", store.getState());

8e438a0fb46497a4d74687abd133c5c15b841088
Donna-Sayos <Donnasayos@gmail.com>
2023-04-23 13:01:10 -0400

    added app js and css file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App";
 import store from "./store";
+
+const rootElement = ReactDOM.createRoot(document.getElementById("root"));
+
+rootElement.render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);

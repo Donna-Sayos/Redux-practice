6d90040370d617dd2a7db039b1c737ca088707a5
Donna-Sayos <Donnasayos@gmail.com>
2023-04-24 13:53:29 -0400

    added queries for my db

--- server/db/queries/todoQueries.js
+++ server/db/queries/todoQueries.js
@@ -1,3 +1,18 @@
-const getTodos = "SELECT * FROM todo";
-const getTodoByID = "SELECT * FROM todo WHERE id = $1";
-const addTodo = "INSERT INTO todo (todo, "
+const getAllTodos_ = "SELECT * FROM todo";
+const getTodoByID_ = "SELECT * FROM todo WHERE id = $1";
+const addTodo_ =
+  "INSERT INTO todo (description, isCompleted) VALUES ($1, false) RETURNING *";
+const removeTodo_ = "DELETE FROM todo WHERE id = $1";
+const updateTodo_ =
+  "UPDATE todo SET description = $1 WHERE id = $2 RETURNING id, description, isCompleted";
+const toggleTodo_ =
+  "UPDATE todo SET isCompleted = NOT isCompleted WHERE id = $1 RETURNING id, description, isCompleted";
+
+module.exports = {
+  getAllTodos_,
+  getTodoByID_,
+  addTodo_,
+  removeTodo_,
+  updateTodo_,
+  toggleTodo_,
+};

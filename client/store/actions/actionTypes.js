a1f51bbd3ca936768bb80fd7eeb4022f843bca8f
Donna-Sayos <Donnasayos@gmail.com>
2023-04-26 14:34:02 -0400

    added a few more action types

--- client/store/actions/actionTypes.js
+++ client/store/actions/actionTypes.js
@@ -1,5 +1,7 @@
 // ACTION TYPES
+export const SET_TODOS = "SET_TODOS";
 export const ADD_TODO = "ADD_TODO";
 export const REMOVE_TODO = "REMOVE_TODO";
 export const TOGGLE_TODO = "TOGGLE_TODO";
 export const UPDATE_TODO = "UPDATE_TODO";
+export const CLEAR_TODOS = "CLEAR_TODOS";

9c04287a0b8955e5085420e7fd25aef41fd5b9f9
Donna-Sayos <Donnasayos@gmail.com>
2023-04-27 11:35:41 -0400

    made map state and map dispatch also used connect

--- client/components/todo/Todo.js
+++ client/components/todo/Todo.js
@@ -1,15 +1,31 @@
 import React from "react";
 import { connect } from "react-redux";
+import {
+  fetchTodos_,
+  fetchSingleTodo_,
+  addTodo_,
+  removeTodo_,
+  toggleTodo_,
+  updateTodo_,
+  clearTodos_,
+} from "../../store/actions/thunks";
 
 function Todo() {
   return <div>Todo</div>;
 }
 
-const mapStateToProps = (state) => ({
-  todo: state.todo,
-  isCompleted: state.isCompleted,
+const mapState = (state) => ({
+  todos: state.todos,
 });
 
+const mapDispatch = (dispatch) => ({
+  fetchTodos: () => dispatch(fetchTodos_()),
+  fetchSingleTodo: (id) => dispatch(fetchSingleTodo_(id)),
+  addTodo: (todo) => dispatch(addTodo_(todo)),
+  removeTodo: (id) => dispatch(removeTodo_(id)),
+  toggleTodo: (id) => dispatch(toggleTodo_(id)),
+  updateTodo: (id, todo) => dispatch(updateTodo_(id, todo)),
+  clearTodos: () => dispatch(clearTodos_()),
+});
 
-
-export default Todo;
+export default connect(mapState, mapDispatch)(Todo);

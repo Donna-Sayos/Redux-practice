b0c2b67fc3fbc7c6486d970005e41f06e87eb59b
Donna-Sayos <Donnasayos@gmail.com>
2023-04-27 11:41:07 -0400

    addec some console logs to check my thunks

--- client/store/actions/thunks.js
+++ client/store/actions/thunks.js
@@ -5,6 +5,7 @@ import Axios from "axios";
 export const fetchTodos_ = () => async (dispatch) => {
   try {
     const { data } = await Axios.get(`/api/v1/todos`);
+    console.log(`fetchTodos triggered`); // TODO: remove later
     dispatch(actionCreators.setTodos(data));
   } catch (err) {
     console.log(`Error in fetchTodos thunk: ${err}`);
@@ -14,6 +15,7 @@ export const fetchTodos_ = () => async (dispatch) => {
 export const fetchSingleTodo_ = (id) => async (dispatch) => {
   try {
     const { data } = await Axios.get(`/api/v1/todos/${id}`);
+    console.log(`fetchSingleTodo triggered`); // TODO: remove later
     dispatch(actionCreators.setTodo(data));
   } catch (err) {
     console.log(`Error in fetchSingleTodo thunk: ${err}`);
@@ -23,6 +25,7 @@ export const fetchSingleTodo_ = (id) => async (dispatch) => {
 export const addTodo_ = (todo) => async (dispatch) => {
   try {
     const { data } = await Axios.post(`/api/v1/todos`, { description: todo });
+    console.log(`addTodo triggered`); // TODO: remove later
     dispatch(actionCreators.addTodo(data));
   } catch (err) {
     console.log(`Error in addTodo thunk: ${err}`);
@@ -32,6 +35,7 @@ export const addTodo_ = (todo) => async (dispatch) => {
 export const removeTodo_ = (id) => async (dispatch) => {
   try {
     const { data } = await Axios.delete(`/api/v1/todos/${id}`);
+    console.log(`removeTodo triggered`); // TODO: remove later
     dispatch(actionCreators.removeTodo(data.id));
   } catch (err) {
     console.log(`Error in removeTodo thunk: ${err}`);
@@ -41,6 +45,7 @@ export const removeTodo_ = (id) => async (dispatch) => {
 export const toggleTodo_ = (id) => async (dispatch) => {
   try {
     const { data } = await Axios.put(`/api/v1/todos/${id}/toggle`);
+    console.log(`toggleTodo triggered`); // TODO: remove later
     dispatch(actionCreators.toggleTodo(data.id));
   } catch (err) {
     console.log(`Error in toggleTodo thunk: ${err}`);
@@ -52,6 +57,7 @@ export const updateTodo_ = (id, todo) => async (dispatch) => {
     const { data } = await Axios.put(`/api/v1/todos/${id}`, {
       description: todo,
     });
+    console.log(`updateTodo triggered`); // TODO: remove later
     dispatch(actionCreators.updateTodo(data.id, data));
   } catch (err) {
     console.log(`Error in updateTodo thunk: ${err}`);
@@ -61,6 +67,7 @@ export const updateTodo_ = (id, todo) => async (dispatch) => {
 export const clearTodos_ = () => async (dispatch) => {
   try {
     await Axios.delete(`/api/v1/todos/clear`);
+    console.log(`clearTodos triggered`); // TODO: remove later
     dispatch(actionCreators.clearTodos());
   } catch (err) {
     console.log(`Error in clearTodos thunk: ${err}`);

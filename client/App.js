24c2583cdaf67127891643a0c9182a07a4bd23b4
Donna-Sayos <Donnasayos@gmail.com>
2023-05-07 20:03:53 -0400

    changed paper and background color

--- client/App.js
+++ client/App.js
@@ -8,7 +8,7 @@ const styles = makeStyles((theme) => ({
     padding: "5px",
     textAlign: "center",
     color: "#faf0e6 !important",
-    backgroundColor: "#4a6741 !important",
+    backgroundColor: "#5f9ea0 !important",
     minWidth: "90vw",
     minHeight: "90vh",
   },
@@ -18,7 +18,7 @@ export default function App() {
   const cssClasses = styles();
 
   return (
-    <Paper className={cssClasses.paper} elevation={9} square>
+    <Paper className={cssClasses.paper} >
       <Todos />
     </Paper>
   );

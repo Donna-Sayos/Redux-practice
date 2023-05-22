94a17c6d696dc41148cc0cd54a7f222cee304df1
Donna-Sayos <Donnasayos@gmail.com>
2023-05-22 13:07:31 -0400

    changed my reducer to add maxhistory to all cases

--- client/store/reducers/todoReducer.js
+++ client/store/reducers/todoReducer.js
@@ -8,7 +8,7 @@ const initialState = {
 
 const MAX_HISTORY = 5;
 
-export const todoReducer = (state = initialState, action) => {
+export const todoReducer = (state = initialState, action, dispatch) => {
   switch (action.type) {
     case actions.SET_TODOS:
       return {
@@ -18,7 +18,7 @@ export const todoReducer = (state = initialState, action) => {
 
     case actions.ADD_TODO:
       let newAddPast = [...state.past, state.present];
-      if (newAddPast.length > MAX_HISTORY) newAddPast.shift(); // remove the first item in the array
+      if (newAddPast.length > MAX_HISTORY) newAddPast = newAddPast.shift(); // remove the first item in the array
       return {
         past: newAddPast,
         present: [...state.present, action.payload.todo],
@@ -26,15 +26,23 @@ export const todoReducer = (state = initialState, action) => {
       };
 
     case actions.REMOVE_TODO:
+      let newRemovedPast = [...state.past, state.present];
+      if (newRemovedPast.length > MAX_HISTORY)
+        newRemovedPast = newRemovedPast.shift();
+
       return {
-        past: [...state.past, state.present],
+        past: newRemovedPast,
         present: state.present.filter((todo) => todo.id !== action.payload.id),
         future: [],
       };
 
     case actions.TOGGLE_TODO:
+      let newToggledPast = [...state.past, state.present];
+      if (newToggledPast.length > MAX_HISTORY)
+        newToggledPast = newToggledPast.shift();
+
       return {
-        past: [...state.past, state.present],
+        past: newToggledPast,
         present: state.present.map((todo) =>
           todo.id === action.payload.id
             ? { ...todo, isCompleted: !todo.isCompleted }
@@ -44,8 +52,12 @@ export const todoReducer = (state = initialState, action) => {
       };
 
     case actions.UPDATE_TODO:
+      let newUpdatedPast = [...state.past, state.present];
+      if (newUpdatedPast.length > MAX_HISTORY)
+        newUpdatedPast = newUpdatedPast.shift();
+
       return {
-        past: [...state.past, state.present],
+        past: newUpdatedPast,
         present: state.present.map((todo) =>
           todo.id === action.payload.id
             ? { ...todo, description: action.payload.description }
@@ -55,8 +67,12 @@ export const todoReducer = (state = initialState, action) => {
       };
 
     case actions.CLEAR_TODOS:
+      let newClearedPast = [...state.past, state.present];
+      if (newClearedPast.length > MAX_HISTORY)
+        newClearedPast = newClearedPast.shift();
+
       return {
-        past: [...state.past, state.present],
+        past: newClearedPast,
         present: [],
         future: [],
       };
